5130781853c9e180c2cd11ea2064749c4f1e40cb
Jeremy Tennant <tennantje@gmail.com>
2016-11-13 21:33:05 +1100

    Upgrade specs to use expect rather than should syntax

--- spec/localbitcoins_spec.rb
+++ spec/localbitcoins_spec.rb
@@ -3,26 +3,25 @@ require 'spec_helper'
 describe 'LocalBitcoins' do
   describe '.new' do
     it 'returns a new client instance' do
-      LocalBitcoins.new.should be_a LocalBitcoins::Client
+      # LocalBitcoins.new.should be_a LocalBitcoins::Client
+      expect(LocalBitcoins.new).to be_a LocalBitcoins::Client
     end
   end
 
   describe '.configure' do
     it 'sets a global configuration options' do
       r = LocalBitcoins.configure(client_id: 'TEST_ID', client_secret: 'TEST_SECRET')
-      r.should be_a Hash
-      r.should have_key(:client_id)
-      r.should have_key(:client_secret)
-      r[:client_id].should eql('TEST_ID')
-      r[:client_secret].should eql('TEST_SECRET')
+      expect(r).to be_a Hash
+      expect(r).to have_key(:client_id)
+      expect(r).to have_key(:client_secret)
+      expect(r[:client_id]).to eql('TEST_ID')
+      expect(r[:client_secret]).to eql('TEST_SECRET')
     end
 
     it 'raises ConfigurationError on invalid config parameter' do
-      proc { LocalBitcoins.configure(nil) }.
-        should raise_error(ArgumentError, "Options hash required.")
+      expect(proc { LocalBitcoins.configure(nil) }).to raise_error(ArgumentError, "Options hash required.")
 
-      proc { LocalBitcoins.configure('foo') }.
-        should raise_error ArgumentError, "Options hash required."
+      expect(proc { LocalBitcoins.configure('foo') }).to raise_error(ArgumentError, "Options hash required.")
     end
   end
 
@@ -33,11 +32,11 @@ describe 'LocalBitcoins' do
 
     it 'returns global configuration options' do
       r = LocalBitcoins.configuration
-      r.should be_a Hash
-      r.should have_key(:client_id)
-      r.should have_key(:client_secret)
-      r[:client_id].should eql('TEST_ID')
-      r[:client_secret].should eql('TEST_SECRET')
+      expect(r).to be_a Hash
+      expect(r).to have_key(:client_id)
+      expect(r).to have_key(:client_secret)
+      expect(r[:client_id]).to eql('TEST_ID')
+      expect(r[:client_secret]).to eql('TEST_SECRET')
     end
   end
 
@@ -48,7 +47,7 @@ describe 'LocalBitcoins' do
 
     it 'resets global configuration options' do
       LocalBitcoins.reset_configuration
-      LocalBitcoins.configuration.should eql({})
+      expect(LocalBitcoins.configuration).to eql({})
     end
   end
 end
